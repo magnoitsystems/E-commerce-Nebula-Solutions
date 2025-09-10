ffecc064e495d85e3ccc58886528ef4fd0814fec
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-10 20:27:13 -0300

    almost finished footer section, rest some details

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-// Footer.tsx
 import React from 'react';
 import styles from './Footer.module.css';
 
@@ -9,26 +8,23 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({ className = "" }) => {
   return (
     <footer className={`${styles.footerContainer} ${className}`}>
-      {/* Header con logo y redes sociales */}
       <div className={styles.footerHeader}>
         <div className={styles.logoSection}>
-          <img src="/oneshop.png" alt="OneShop" className={styles.logoIcon} />
-          <h1 className={styles.logoText}>OneShop</h1>
+          <img src="/logos/oneshop.png" alt="OneShop"/>
         </div>
         
         <div className={styles.socialSection}>
-          <p className={styles.socialText}>Seguinos en</p>
-          <div className={styles.socialIcons}>
-            <img src="/instagram.png" alt="Instagram" className={styles.socialIcon} />
-            <img src="/facebook.png" alt="Facebook" className={styles.socialIcon} />
-            <img src="/twitter.png" alt="Twitter" className={styles.socialIcon} />
-          </div>
-        </div>
+  <p className={styles.socialText}>Seguinos en</p>
+  <div className={styles.socialIcons}>
+    <img src="/logos/instagram.png" alt="Instagram" className={styles.socialIcon} />
+    <img src="/logos/facebook.png" alt="Facebook" className={styles.socialIcon} />
+    <img src="/logos/twitter.png" alt="Twitter" className={styles.socialIcon} />
+  </div>
+</div>
+
       </div>
 
-      {/* Contenido principal */}
       <div className={styles.footerContent}>
-        {/* Sobre nosotros */}
         <div className={styles.footerSection}>
           <h3 className={styles.sectionTitle}>Sobre nosotros</h3>
           <p className={styles.aboutText}>
@@ -38,7 +34,6 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
           </p>
         </div>
 
-        {/* Productos */}
         <div className={styles.footerSection}>
           <h3 className={styles.sectionTitle}>Productos</h3>
           <ul className={styles.linksList}>
@@ -51,7 +46,6 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
           </ul>
         </div>
 
-        {/* Contacto */}
         <div className={styles.footerSection}>
           <h3 className={styles.sectionTitle}>Contacto</h3>
           <div className={styles.contactInfo}>
@@ -60,7 +54,6 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
           </div>
         </div>
 
-        {/* Unirte */}
         <div className={styles.footerSection}>
           <h3 className={styles.sectionTitle}>Unirte</h3>
           <ul className={styles.linksList}>
@@ -69,7 +62,6 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
           </ul>
         </div>
 
-        {/* Comprar */}
         <div className={styles.footerSection}>
           <h3 className={styles.sectionTitle}>Comprar</h3>
           <ul className={styles.linksList}>
@@ -79,7 +71,6 @@ const Footer: React.FC<FooterProps> = ({ className = "" }) => {
           </ul>
         </div>
 
-        {/* Sección CTA */}
         <div className={`${styles.footerSection} ${styles.ctaSection}`}>
           <div className={styles.ctaText}>
             <h4>¿Tienes preguntas?</h4>
