6028a8e08dd76ecb9686e0fabe2e9e82fc6a358f
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-12 14:51:27 -0300

    improve products grid logic delegating responsibilities for loading products

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,20 @@
 // import File from "../src/component/File/File.tsx";
 import './App.css'
-import ProductList from './component/AdminProductsCard/ProductList.tsx';
-import CategoryCard from './component/CategoryCard/CategoryCard.tsx';
+// import ProductList from './component/AdminProductsCard/ProductList.tsx';
+// import CategoryCard from './component/CategoryCard/CategoryCard.tsx';
 import Footer from "./component/Footer/Footer.tsx";
 import ProductGrid from './component/ProductCard/ProductGrid.tsx';
 import PurchaseList from './component/PurchaseList/PurchaseList.tsx';
 
 function App() {
-
-  const products = [
-    {
-      id: 1,
-      name: "Sombras de ojos",
-      image: "/test-images/maybelline-combo.jpg",
-      price: 15000,
-      discountPrice: 10000,
-      discountPercent: 25,
-    },
-    {
-      id: 2,
-      name: "Sombras de ojos",
-      image: "/test-images/maybelline-combo.jpg",
-      price: 15000,
-    },
-    {
-      id: 3,
-      name: "Sombras de ojos",
-      image: "/test-images/maybelline-combo.jpg",
-      price: 20000,
-    },
-  ];
-
   return (
-    // <File/>
     <main>
-      {/* <CategoryCard></CategoryCard>
-      <ProductList></ProductList> */}
-      <PurchaseList></PurchaseList>
-      <ProductGrid products={products} />
-      <Footer></Footer>
+      <PurchaseList />
+      <ProductGrid />
+      <Footer />
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/component/ProductCard/ProductGrid.tsx
+++ src/component/ProductCard/ProductGrid.tsx
@@ -1,20 +1,30 @@
 import ProductCard from "./ProductCard";
 import styles from "./ProductGrid.module.css";
 
-interface Product {
-  id: number;
-  name: string;
-  image: string;
-  price: number;
-  discountPrice?: number;
-  discountPercent?: number;
-}
+const products = [
+  {
+    id: 1,
+    name: "Sombras de ojos",
+    image: "/test-images/maybelline-combo.jpg",
+    price: 15000,
+    discountPrice: 10000,
+    discountPercent: 25,
+  },
+  {
+    id: 2,
+    name: "Sombras de ojos",
+    image: "/test-images/maybelline-combo.jpg",
+    price: 15000,
+  },
+  {
+    id: 3,
+    name: "Sombras de ojos",
+    image: "/test-images/maybelline-combo.jpg",
+    price: 20000,
+  },
+];
 
-interface Props {
-  products: Product[];
-}
-
-export default function ProductGrid({ products }: Props) {
+export default function ProductGrid() {
   return (
     <div className={styles.grid}>
       {products.map((p) => (
