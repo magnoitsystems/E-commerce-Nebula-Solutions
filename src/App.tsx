5fac83906affb5c6d703b9ffb56099f60fb3cac8
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-13 15:21:54 -0300

    create first section home using browser routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import './App.css'
-import Footer from "./component/Footer/Footer.tsx";
-import ProductGrid from './component/ProductCard/ProductGrid.tsx';
-import NavBar from "./component/NavBar/NavBar.tsx";
-import Carrusel from "./component/File/Carrusel/Carrusel.tsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
 
 function App() {
-    const images = [
-        "/carruselImages/publi/Coca-Cola-nueva-imagen-1280x720.jpg",
-        "/carruselImages/publi/spotify-podcast-1.webp"
-    ];
-
-    return (
-      <main>
-          <NavBar/>
-          <Carrusel images={images} interval={2000}/>
-          <ProductGrid />
-          <Footer />
-      </main>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import Footer from "../component/Footer/Footer.tsx";
+import ProductGrid from '../component/ProductCard/ProductGrid.tsx';
+import NavBar from "../component/NavBar/NavBar.tsx";
+import Carrusel from "../component/File/Carrusel/Carrusel.tsx";
+
+function Home() {
+    const images = [
+        "/carruselImages/publi/Coca-Cola-nueva-imagen-1280x720.jpg",
+        "/carruselImages/publi/spotify-podcast-1.webp"
+    ];
+
+    return (
+      <main>
+          <NavBar/>
+          <Carrusel images={images} interval={2000}/>
+          <ProductGrid />
+          <Footer />
+      </main>
+    );
+}
+
+export default Home;
+
