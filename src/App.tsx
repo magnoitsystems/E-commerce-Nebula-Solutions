db6e1ec5997a0786c6799bb5012633109e54e3d9
Agostina Bidegain <agostinabidegain@gmail.com>
2025-09-13 13:15:13 -0300

    implemented desktop nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import './App.css'
 import Footer from "./component/Footer/Footer.tsx";
 import ProductGrid from './component/ProductCard/ProductGrid.tsx';
 import PurchaseList from './component/PurchaseList/PurchaseList.tsx';
+import NavBar from "./component/NavBar/NavBar.tsx";
 
 function App() {
   return (
     <main>
+        <NavBar/>
       <PurchaseList />
       <ProductGrid />
       <Footer />
