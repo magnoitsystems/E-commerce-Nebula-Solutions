b3436cb3cf3ecec2ee102b7e5db15e65c1b40cb5
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-13 14:48:26 -0300

    add mobile first design in product card and product card grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-// import File from "../src/component/File/File.tsx";
 import './App.css'
-// import ProductList from './component/AdminProductsCard/ProductList.tsx';
-// import CategoryCard from './component/CategoryCard/CategoryCard.tsx';
 import Footer from "./component/Footer/Footer.tsx";
 import ProductGrid from './component/ProductCard/ProductGrid.tsx';
-import PurchaseList from './component/PurchaseList/PurchaseList.tsx';
 import NavBar from "./component/NavBar/NavBar.tsx";
 import Carrusel from "./component/File/Carrusel/Carrusel.tsx";
 
@@ -18,7 +14,6 @@ function App() {
       <main>
           <NavBar/>
           <Carrusel images={images} interval={2000}/>
-          <PurchaseList />
           <ProductGrid />
           <Footer />
       </main>
