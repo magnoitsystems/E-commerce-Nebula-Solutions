7fb3ee36407abc40d0e8bb327f76566d4757f0b4
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-14 20:25:17 -0300

    add mobile first por purchase card and purchases list

--- src/pages/Purchases.tsx
+++ src/pages/Purchases.tsx
@@ -1,10 +1,12 @@
 import Footer from "../component/Footer/Footer";
 import NavBar from "../component/NavBar/NavBar";
+import PurchaseList from "../component/PurchaseList/PurchaseList";
 
 function Purchases(){
     return(
           <main>
           <NavBar/>
+          <PurchaseList></PurchaseList>
           <Footer />
       </main>
     );
