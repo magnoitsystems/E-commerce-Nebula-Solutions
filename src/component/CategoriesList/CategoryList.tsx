cbdfd2a5941dbd743c382c6f25b02e55160b0b98
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-16 20:43:06 -0300

    fix problem with subcategories deployment, rest fix details

--- src/component/CategoriesList/CategoryList.tsx
+++ src/component/CategoriesList/CategoryList.tsx
@@ -1,94 +1,85 @@
-// src/components/CategoryList.tsx
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./CategoryList.module.css";
 
 interface Category {
   id: number;
   name: string;
   image: string;
-  subcategories: string[];
+  subcategories?: string[];
 }
 
 const categories: Category[] = [
   {
     id: 1,
     name: "Maquillaje",
-    image: "/test-images/falsies.jpg",
+    image: "/test-images/makeup.jpg",
     subcategories: ["Artístico", "Fantasía", "Social", "Efectos especiales"],
   },
   {
     id: 2,
     name: "Deportivo",
     image: "/test-images/sports.jpg",
-    subcategories: ["Fútbol", "Básquet", "Tenis", "Running"],
+    subcategories: ["Fútbol", "Running", "Natación"],
   },
   {
     id: 3,
     name: "Electrodomésticos",
     image: "/test-images/electro.jpg",
-    subcategories: ["Cocina", "Lavarropas", "Climatización"],
+    subcategories: ["Cocina", "Limpieza", "Climatización"],
   },
   {
     id: 4,
-    name: "Herramientas y útiles",
-    image: "/test-images/tools.jpg",
-    subcategories: ["Manuales", "Eléctricas", "Medición"],
+    name: "Maquillaje",
+    image: "/test-images/makeup.jpg",
+    subcategories: ["Artístico", "Fantasía", "Social", "Efectos especiales"],
   },
   {
     id: 5,
-    name: "Indumentaria",
-    image: "/test-images/clothes.jpg",
-    subcategories: ["Hombre", "Mujer", "Niños"],
+    name: "Deportivo",
+    image: "/test-images/sports.jpg",
+    subcategories: ["Fútbol", "Running", "Natación"],
   },
   {
     id: 6,
-    name: "Decoración",
-    image: "/test-images/deco.jpg",
-    subcategories: ["Living", "Dormitorio", "Exterior"],
-  },
-  {
-    id: 7,
-    name: "Vehículos",
-    image: "/test-images/cars.jpg",
-    subcategories: ["Autos", "Motos", "Camionetas"],
-  },
-  {
-    id: 8,
-    name: "Accesorios",
-    image: "/test-images/accessories.jpg",
-    subcategories: ["Relojes", "Carteras", "Cinturones"],
-  },
-  {
-    id: 9,
-    name: "Mochilas y carry ons",
-    image: "/test-images/bags.jpg",
-    subcategories: ["Mochilas", "Valijas", "Bolsos"],
+    name: "Electrodomésticos",
+    image: "/test-images/electro.jpg",
+    subcategories: ["Cocina", "Limpieza", "Climatización"],
   },
 ];
 
 const CategoryList: React.FC = () => {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+
   return (
     <div className={styles.categoryList}>
-      {categories.map((cat) => (
-        <div key={cat.id} className={styles.categoryCard}>
-          <div className={styles.categoryHeader}>
-            <img
-              src={cat.image}
-              alt={cat.name}
-              className={styles.categoryImage}
-            />
-            <span className={styles.categoryName}>{cat.name}</span>
-          </div>
+      {categories.map((cat, index) => {
+        const isOpen = openIndex === index;
+        return (
+          <div
+            key={cat.id}
+            className={styles.categoryCard}
+            onMouseEnter={() => setOpenIndex(index)}
+            onMouseLeave={() => setOpenIndex(null)}
+          >
+            <div className={styles.categoryHeader}>
+              <img src={cat.image} alt={cat.name} className={styles.categoryImage} />
+              <span className={styles.categoryName}>{cat.name}</span>
+            </div>
 
-          <ul className={styles.subcategories}>
-            {cat.subcategories.map((sub, i) => (
-              <li key={i} className={styles.subcategoryItem}>
-                {sub}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+            {cat.subcategories && (
+              <ul className={`${styles.subcategories} ${isOpen ? styles.show : ""}`}>
+                {cat.subcategories.map((sub, subIndex) => (
+                  <li key={subIndex} className={styles.subcategoryItem}>
+                    {sub}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
