9f68e10f2e23d65e8aa138823817fdd6340ce061
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-22 16:03:29 -0300

    finish instalation of inertia and fix things in composer json (was uncomplete)

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    
+    @routes
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
