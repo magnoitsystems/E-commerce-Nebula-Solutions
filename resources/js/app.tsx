88ae48caa3f23428eb7e5dcbc61e1d28d257898a
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-21 22:25:46 -0300

    start intertia js configuration, still with a few errors to compile

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,31 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home';
-import FileProduct from './Pages/FileProduct';
-import Admin from './Pages/Admin';
-import Purchases from './Pages/Purchases';
-import Login from './Pages/Login';
-import Logup from "./Pages/Logup";
-import Cart from "./Pages/Cart";
-import AllProducts from './Pages/AllProducts';
-import ProductsByCategory from './Pages/ProductsByCategory';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { InertiaApp } from "@inertiajs/inertia-react";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='/fileProduct' element={<FileProduct image={[]} />} />
-          <Route path="/admin" element={<Admin />}></Route>
-          <Route path="/purchases" element={<Purchases />}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/logup" element={<Logup/>}></Route>
-          <Route path="/micarrito" element={<Cart/>}></Route>
-          <Route path="/products" element={<AllProducts/>}></Route>
-          <Route path="/productsByCategory/:category" element={<ProductsByCategory/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const el = document.getElementById("app");
 
-export default App;
+createRoot(el!).render(
+  <React.StrictMode>
+    <InertiaApp
+      initialPage={JSON.parse(el!.dataset.page!)}
+      resolveComponent={(name: string) => import(`./Pages/${name}`).then((module) => module.default)} initialComponent={undefined}/>
+  </React.StrictMode>
+);
