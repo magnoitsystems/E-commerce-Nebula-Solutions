4afdc41471cad6dbad638254234aac7736b8d8fa
milagros alvarez <milagrosalvarez2604@gmail.com>
2025-09-24 09:45:32 -0300

    PROBLEM ALMOST SOLVED, NOT EMPTY HTML ANYMORE, VIEW A FEW COMPONENTS IN HOME SECTION

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from '@inertiajs/react';
+import { BrowserRouter } from "react-router-dom";
 
 createInertiaApp({
   resolve: name => import(`./Pages/${name}.tsx`).then(module => module.default),
   setup({ el, App, props }) {
     createRoot(el).render(
       <React.StrictMode>
-        <App {...props} />
+        <BrowserRouter>
+          <App {...props} />
+        </BrowserRouter>
       </React.StrictMode>
     );
   },
